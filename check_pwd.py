379817fda848cb2e608f9f03ec7a3151a1157762
Hieu Ngo <ngoh@oregonstate.edu>
2024-02-19 22:25:28 -0800

    Changed check_pwd to allow test6 to pass the fail test if there are no symbols in password. It passed.

--- check_pwd.py
+++ check_pwd.py
@@ -5,11 +5,16 @@ def check_pwd(pwd):
         return False
     lower_count = 0
     upper_count = 0
+    symbols = "~`!@#$%^&*()_+-="
+    symbol_count = 0
     for letter in pwd:
         if letter.islower():
             lower_count += 1
         if letter.isupper():
             upper_count += 1
-    if lower_count == 0 or upper_count == 0:
+        for character in symbols:
+            if letter == character:
+                symbol_count += 1
+    if lower_count == 0 or upper_count == 0 or symbol_count == 0:
         return False
     return True
