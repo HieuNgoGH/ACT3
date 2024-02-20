9497eea5483e55c9e227a656bb6aeca3373d3e0d
Hieu Ngo <ngoh@oregonstate.edu>
2024-02-19 22:16:03 -0800

    Changed check_pwd to return False if password does not contain any uppercase letter in test4 to fail. It passed.

--- check_pwd.py
+++ check_pwd.py
@@ -4,9 +4,12 @@ def check_pwd(pwd):
     if len(pwd) > 20:
         return False
     lower_count = 0
+    upper_count = 0
     for letter in pwd:
         if letter.islower():
             lower_count += 1
-    if lower_count == 0:
+        if letter.isupper():
+            upper_count += 1
+    if lower_count == 0 or upper_count == 0:
         return False
     return True
