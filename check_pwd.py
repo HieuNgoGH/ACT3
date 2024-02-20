afd1a175ea8d3731150da5d06097016ef3d8c5ea
Hieu Ngo <ngoh@oregonstate.edu>
2024-02-19 22:34:18 -0800

    CHanged check_pwd to filter through password and check for digits and if none to return False. It passed no failures of tests.

--- check_pwd.py
+++ check_pwd.py
@@ -3,11 +3,14 @@ def check_pwd(pwd):
         return False
     if len(pwd) > 20:
         return False
+    digit_count = 0
     lower_count = 0
     upper_count = 0
     symbols = "~`!@#$%^&*()_+-="
     symbol_count = 0
     for letter in pwd:
+        if letter.isdigit():
+            digit_count += 1
         if letter.islower():
             lower_count += 1
         if letter.isupper():
@@ -15,6 +18,7 @@ def check_pwd(pwd):
         for character in symbols:
             if letter == character:
                 symbol_count += 1
-    if lower_count == 0 or upper_count == 0 or symbol_count == 0:
+    if lower_count == 0 or upper_count == 0 or symbol_count == 0 or digit_count == 0 or digit_count == len(pwd):
         return False
+
     return True
